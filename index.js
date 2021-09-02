3b58e52fe113118e24d843591daddba93886fd62
bwesterg <benton.westergaard@gmail.com>
2021-09-02 14:41:20 -0600

    Destructured name, image so the app runs faster. In theory.

--- index.js
+++ index.js
@@ -21,20 +21,32 @@ function removeLoadingGif() {
 }
 
 function showPokemon(pokemon) {
-    const pokemonCard = document.createElement('div');
-    pokemonCard.classList.add('pokemon-card');
-
-    const pokemonName = document.createElement('h2');
-    pokemonName.textContent = pokemon.name;
-
-    // console.log('pokemon', pokemon)
-
-    const pokemonImage = document.createElement('img')
-    pokemonImage.src = pokemon.sprites.other["official-artwork"].front_default
+    const pokemonCard = createPokemonCard();
+    const pokemonName = createPokemonCard(pokemon);
+    const pokemonImage = createPokemonImage(pokemon);
 
     pokemonCard.append(pokemonName, pokemonImage);
     pokemonContainer.append(pokemonCard);
 }
+
+function createPokemonCard() {
+    const pokemonCard = document.createElement('div');
+    pokemonCard.classList.add('pokemon-card');
+    return pokemonCard;
+}
+
+function createPokemonName({ name }) {
+    const pokemonName = document.createElement('h2');
+    pokemonName.textContent = name;
+    return pokemonName;
+}
+
+function createPokemonImage({ sprites }) {
+    const pokemonImage = document.createElement('img')
+    pokemonImage.src = sprites.other["official-artwork"].front_default;
+    return pokemonImage;
+}
+
 
 function parseJSON(response) {
     return response.json();
